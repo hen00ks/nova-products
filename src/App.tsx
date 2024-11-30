f8a4e17c7bd236b4ca0ba5ee231bba1002e432e3
hen00ks <henas2272@gmail.com>
2024-11-30 11:15:41 +0300

    FEATURE| added react router dom and added pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,20 @@
 // All packages except `@mantine/hooks` require styles imports
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Products from "./pages/Products";
 
 export default function App() {
-  return <MantineProvider>App</MantineProvider>;
+  return (
+    <MantineProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/details" element={<h1>Details page</h1>} />
+          <Route path="/reviews" element={<h1>Reviews page</h1>} />
+          <Route path="/new" element={<h1>Create new product page</h1>} />
+        </Routes>
+      </Router>
+    </MantineProvider>
+  );
 }
