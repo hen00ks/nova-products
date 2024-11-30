de5b1e1874bfbe984b8d15a1e31bd9fa4507033c
hen00ks <henas2272@gmail.com>
2024-11-30 12:06:52 +0300

    PROGRESS| added a fetch for products

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,5 +1,28 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Products() {
-  return <div>Products</div>;
+  const { data: productsObject, isLoading } = useQuery({
+    queryKey: ["products"],
+    queryFn: () =>
+      fetch("https://test-api.nova-techs.com/products").then((res) =>
+        res.json()
+      ),
+  });
+
+  if (isLoading) return <h2>Loading...</h2>;
+
+  console.log(productsObject);
+
+  const reacted = productsObject?.data.map((product, i) => {
+    return (
+      <div key={product.id}>
+        <img src={product.imageUrls[0]} alt="product image" />
+        <h3>{product.name}</h3>
+        <h2>{product.price}</h2>
+        <p>{product.use}</p>
+      </div>
+    );
+  });
+
+  return <div>{reacted}</div>;
 }
